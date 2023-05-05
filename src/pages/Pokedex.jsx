2c10aa7cc49c5565af62ea6bbe88cb55b1e6a3c7
waltercp <walter_cp_96@hotmail.com>
2023-05-04 23:26:50 -0600

    carga

--- src/pages/Pokedex.jsx
+++ src/pages/Pokedex.jsx
@@ -4,28 +4,27 @@ import ForPoke from '../components/Pokedex/ForPoke'
 import PokeContainer from '../components/Pokedex/PokeContainer'
 import useFetch from '../hooks/useFetch'
 import PokeType from '../components/Pokedex/PokeType'
-import '../styles/pokedex.css'
 import PokeHeader from '../components/Home/PokeHeader'
 import Pagination from '../components/Pokedex/Pagination'
-
+import '../styles/pokedex.css'
 
 const Pokedex = () => {
-
-
   const [pokemons, setPokemons] = useState()
   const [pokeSearch, setPokeSearch] = useState()
-  const [postPerPage, setPostPerPage] = useState(900)
+  const [postPerPage, setPostPerPage] = useState(900) //Numero de Pokemones
   const [optionType, setOptionType] = useState('All')
 
-  const [forPoke, setForPoke] = useState(false)
-  const [pokeType, setPokeType] = useState(false)
+  const [forPoke, setForPoke] = useState(false) //validar pokemones por Busquedad
+  const [pokeType, setPokeType] = useState(false) //validar pokemones por tipo
 
-  const [pagePokemon, setPagePokemon] = useState(30)
+  const [pagePokemon, setPagePokemon] = useState(30) //Numero de Pokemones por pagina
   const [currentPage, setCurrentPage] = useState(1)
-
+  const [hasError, setHasError] = useState(false) //mensaje de error
+  const [isLoading, setIsLoading] = useState(false);
 
 
   useEffect(() => {
+    setIsLoading(true);
     if (optionType !== 'All' && pokeType) {
       const url = `https://pokeapi.co/api/v2/type/${optionType}`;
 
@@ -34,31 +33,56 @@ const Pokedex = () => {
         res => {
           const arr = res.data.pokemon.map(e => e.pokemon);
           setPokemons({ results: arr });
+          setHasError(false)
+          setIsLoading(false);
         },
         err => console.log(err)
       );
 
     } else if (pokeSearch && forPoke) {
-
       const url = `https://pokeapi.co/api/v2/pokemon/${pokeSearch}`
-      const obj = {
-        results: [{ url }]
-      }
-      setPokemons(obj)
-    } else {
+      useFetch(
+        url,
+        res => {
+          const obj = {
+            results: [{ url }]
+          }
+          setPokemons(obj)
+          setHasError(false)
+          setIsLoading(false);
+        },
+        err => {
+          console.log(err)
+          setIsLoading(false);
+          setHasError(true)
+          setTimeout(() => {
+            setHasError(false)
+            
+          }, 1800)
+        }
+      );
 
+    } else {
       const url = `https://pokeapi.co/api/v2/pokemon?limit=${postPerPage}&offset=0`;
       useFetch(
         url,
-        res => setPokemons(res.data),
+        res => {
+          setPokemons(res.data)
+          setHasError(false)
+          setIsLoading(false);
+        },
         err => console.log(err)
       );
     }
 
-  }, [pokeSearch, optionType, pokeType]);
+  }, [pokeSearch, optionType]);
 
+  console.log(hasError)
+
+
+  //Paginacion
   const lastIndex = currentPage * pagePokemon
-    const firstIndex = lastIndex - pagePokemon
+  const firstIndex = lastIndex - pagePokemon
 
 
   const { trainerName } = useSelector(state => state)
@@ -82,30 +106,40 @@ const Pokedex = () => {
         </div>
 
       </div>
-      <Pagination   
+      <Pagination
         pagePokemon={pagePokemon}
         currentPage={currentPage}
         setCurrentPage={setCurrentPage}
         poblacion={pokemons?.results.length}
-        
       />
 
       <div className='cards-container'>
         <div className='cards-container-poke'>
           {
-            pokemons?.results.map(pokemon => (
-              <PokeContainer
-                key={pokemon.url}
-                url={pokemon.url}
+             isLoading
+              ? <h1>cargando</h1>
 
-              />
+              : <>
+                {
+                  hasError
+                    ? <h1 className='app_error'>❌ Hey! you must provide an id from 1 to 126😫</h1>
+                    : <>
+                      {
+                        pokemons?.results.map(pokemon => (
+                          <PokeContainer
+                            key={pokemon.url}
+                            url={pokemon.url}
+                          />
+                        )).slice(firstIndex, lastIndex)
+                      }
+                    </>
+                }
 
-            )).slice(firstIndex, lastIndex)
+              </>
+
           }
-
         </div>
       </div>
-
     </div>
   )
 }
